789290b3befaa34e313aebf4f7f6e1bf27e0ae59
MrKeke <alexgraf0701@gmail.com>
2023-12-14 13:20:37 +0300

    gg

--- frontend/src/js/cabinet.js
+++ frontend/src/js/cabinet.js
@@ -263,8 +263,6 @@ async function load() {
             lessonModal.style.display = 'block'
             const responseComment = await fetch(`https://lagzya.top:8675/comment/${openedLesson}`);
             const data = await responseComment.json();
-
-            console.log(data)
             clearDiv('commentContainer')
             data.lessons.forEach(([author, comment]) => {
                 const div = document.createElement('div')
