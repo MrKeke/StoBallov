1e9bc4b911b298d23b577bfa03cfa983c47d2852
mrkeke <alexgraf0701@gmail.com>
2024-04-22 13:31:23 +0300

    fix build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [tailwindcss],
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                tarif: 'tarif.html',
+                cabinet: 'cabinet.html',
+                auth: 'auth.html',
+            },
+        }
+    }
 })
